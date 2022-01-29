04bc9a9375521f5b6ba6ee4cc830b27c2fd6f14f
littlesquirt1 <codyc9999@gmail.com>
2022-01-28 19:09:36 -0500

    Changed to Hi, World!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var port = process.env.PORT;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('Hi, World!');
 }
 
 const server = http.createServer(requestListener);
