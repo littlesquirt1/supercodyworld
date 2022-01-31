d9df993e0d0ba353de45b1a6c6eb511318110405
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-01-31 17:04:23 +0000

    express.js

--- index.js
+++ index.js
@@ -1,59 +1,13 @@
-// var http = require('http'),
-// fs = require('fs');
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 8080;
 
-// fs.readFile('./index.html', function (err, html) {
-//     if (err) {
-//         throw err; 
-//     }       
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(process.env.PORT);
-// });
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
-var http = require('http');
-var url = require('url');
-
-var server = http.createServer(function(req,res){
-    var pathname = url.parse(req.url).pathname;
-    switch(pathname){
-        case '/subpage':
-            //top
-            fs.readFile('./game.html', null, function (error, data) {
-              if (error) {
-                response.writeHead(404);
-                respone.write('Whoops! File not found!');
-              } else {
-                response.write(data);
-              }
-              respone.end();
-            });
-            //bottom
-        break;
-        default:
-            fs.readFile('./index.html', null, function (error, data) {
-              if (error) {
-                response.writeHead(404);
-                respone.write('Whoops! File not found!');
-              } else {
-                response.write(data);
-              }
-              respone.end();
-            });
-        break;
-    }
-
-}).listen(process.env.PORT);
-
-
-// fs.readFile('./index.html', null, function (error, data) {
-//     if (error) {
-//         response.writeHead(404);
-//         respone.write('Whoops! File not found!');
-//     } else {
-//         response.write(data);
-//     }
-//     response.end();
-// });
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
