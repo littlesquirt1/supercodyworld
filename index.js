94ae8859055ce5e3290f152f0f0f36ef9945a3d3
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-01-31 16:55:18 +0000

    added subpage

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
-var http = require('http'),
-fs = require('fs');
+// var http = require('http'),
+// fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(process.env.PORT);
-});
+// fs.readFile('./index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(process.env.PORT);
+// });
+
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req,res){
+    var pathname = url.parse(req.url).pathname;
+    switch(pathname){
+        case '/subpage':
+            //top
+            fs.readFile('./game.html', null, function (error, data) {
+              if (error) {
+                response.writeHead(404);
+                respone.write('Whoops! File not found!');
+              } else {
+                response.write(data);
+              }
+              respone.end();
+            });
+            //bottom
+        break;
+        default:
+            fs.readFile('./index.html', null, function (error, data) {
+              if (error) {
+                response.writeHead(404);
+                respone.write('Whoops! File not found!');
+              } else {
+                response.write(data);
+              }
+              respone.end();
+            });
+        break;
+    }
+
+}).listen(process.env.PORT);
+
+
+// fs.readFile('./index.html', null, function (error, data) {
+//     if (error) {
+//         response.writeHead(404);
+//         respone.write('Whoops! File not found!');
+//     } else {
+//         response.write(data);
+//     }
+//     response.end();
+// });
